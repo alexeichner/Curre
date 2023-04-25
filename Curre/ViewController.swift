df2f971464d3d47c28391b13a9160463ca930e3f
alexeichner <eichneralex3@gmail.com>
2023-04-25 13:14:30 -0400

    added timer

--- Curre/ViewController.swift
+++ Curre/ViewController.swift
@@ -9,13 +9,48 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var timerLabel: UILabel!
+    @IBOutlet weak var startButton: UIButton!
+    var timer = Timer()
+    var count = 0
+    var isCounting = false
     override func viewDidLoad() {
         super.viewDidLoad()
-        print("hello")
-        print("hi")
-        print("cool")
+        
     }
 
-
+    @IBAction func startWorkout(_ sender: Any) {
+        if isCounting {
+            isCounting = false
+            timer.invalidate() //stops the timer
+            startButton.setTitle("START", for: .normal) //changes button text to start
+        } else {
+            isCounting = true
+            startButton.setTitle("STOP", for: .normal)
+            timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(timerCounter), userInfo: nil, repeats: true)
+        }
+    }
+    
+    @objc func timerCounter() -> Void {
+        count = count + 1
+        let time = formatTime(seconds: count)
+        let timeString = formatTimeString(hours: time.0, minutes: time.1, seconds: time.2)
+        timerLabel.text = timeString
+    }
+    
+    func formatTime(seconds: Int) -> (Int, Int, Int) {
+        return (seconds / 3600, (seconds % 3600) / 60, (seconds % 3600) % 60)
+    }
+    
+    func formatTimeString(hours: Int, minutes: Int, seconds: Int) -> String {
+        var timeString = ""
+        timeString += String(format: "%02d", hours)
+        timeString += " : "
+        timeString += String(format: "%02d", minutes)
+        timeString += " : "
+        timeString += String(format: "%02d", seconds)
+        return timeString
+    }
+    
 }
 
