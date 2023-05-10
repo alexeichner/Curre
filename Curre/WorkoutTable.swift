c5eb77fe4dc54ca9883ee803bc1f5df693e2726b
alexeichner <eichneralex3@gmail.com>
2023-05-10 13:52:47 -0400

    hhhh

--- Curre/WorkoutTable.swift
+++ Curre/WorkoutTable.swift
@@ -23,6 +23,31 @@ class WorkoutTable: UITableViewController {
         let workoutDefaults = defaults.array(forKey: "workouts")
     }
     
+    func setWorkout<Workout>(_ workout: Workout, forKey: String) where Workout: Encodable
+    {
+        let encoder = JSONEncoder()
+        do {
+            let data = try encoder.encode(workout)
+            set(data, forKey: forKey)
+            synchronize()
+        } catch let encodeErr {
+            print("Failed to encode object:", encodeErr)
+        }
+    }
+        
+    func getWorkout<Workout>(forKey: String, castTo type: Workout.Type) -> Workout? where Workout: Decodable
+    {
+        guard let data = data(forKey: forKey) else { return nil }
+        let decoder = JSONDecoder()
+        do {
+            let workout = try decoder.decode(type, from: data)
+            return workout
+        } catch let decodeError{
+            print("Failed to decode object:" , decodeError)
+            return nil
+        }
+    }
+    
 //    func getCoreData() {
 //
 //
