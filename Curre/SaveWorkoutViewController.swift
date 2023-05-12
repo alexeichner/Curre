c7660f61169c6b2220d17c67c0cb6b043bd48d53
alexeichner <eichneralex3@gmail.com>
2023-05-12 08:29:32 -0400

    ggg

---
+++ Curre/SaveWorkoutViewController.swift
@@ -0,0 +1,17 @@
+//
+//  SaveWorkoutViewController.swift
+//  Curre
+//
+//  Created by Student on 5/11/23.
+//
+
+import Foundation
+import UIKit
+import CoreData
+
+class SaveWorkoutViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
